eb3f5d9076c54f18ab06238dd70f8612a39dfeb9
Elijah Ozhmegov <ElijahOzhmegov@gmail.com>
2021-01-04 20:19:59 +0100

    fix age problem

--- main.R
+++ main.R
@@ -1,23 +1,27 @@
-library(tidyverse)
-library(lubridate)
-library(magrittr)
-library(stringdist)
-library(recipes)
-options(max.print=10000)
+{
+    library(tidyverse)
+    library(lubridate)
+    library(magrittr)
+    library(stringdist)
+    library(recipes)
+    options(max.print=10000)
 
-# What's the most important factors to define the necessity of mental treatment?
-# Is it possible to build any ML model based on those feature to evaluate the risk of mental health problems?
+    # What's the most important factors to define the necessity of mental treatment?
+    # Is it possible to build any ML model based on those feature to evaluate the risk of mental health problems?
 
-#
-# so Target variable is `treatment`
-#
+    #
+    # so Target variable is `treatment`
+    #
 
-df = read.csv(file=here::here("data/survey.csv"))
+    df = read.csv(file=here::here("data/survey.csv"))
+}
 df %>% str(max.level=1)
 
-df = df %>% 
-    rename_all(function(.name){ .name %>% tolower() }) %>% 
-    mutate(timestamp = as_datetime(timestamp)) 
+{
+    df = df %>% 
+        rename_all(function(.name){ .name %>% tolower() }) %>% 
+        mutate(timestamp = as_datetime(timestamp)) 
+}
 
 df$mental_health_interview %>% levels()
 df$comments %>% levels()
@@ -33,7 +37,8 @@ gender_table = df %>%
     summarise(n = n()) %>% 
     arrange(desc(n))
 
-{ # TODO: transform gender to Male/Female Format
+{ 
+    # TODO: transform gender to Male/Female Format
 
 
     normalize_gender <- function(input){
@@ -63,6 +68,32 @@ gender_table = df %>%
                gender = as.factor(gender)) 
 
 }
+
+{ # TODO: remove outlier entries by age
+
+    df %>% 
+        filter(age > 0) %>% 
+        filter(age < 100) %>% 
+        group_by(age) %>% 
+        summarise(n = n()) %>% 
+        arrange(desc(n)) %>% 
+        ggplot(aes(age, n)) +
+        geom_point() +
+        geom_line()
+
+    df %<>% 
+        filter(age > 17) %>% 
+        filter(age < 100)  
+
+}
+
+age = df %>% 
+    group_by(age) %>% 
+    summarise(n = n()) %>% 
+    arrange(desc(age))  
+df %>% nrow()
+
+df$age
 
 df$gender  %>% levels()
 
@@ -98,65 +129,69 @@ df %>%
 # we will use "None"
 #
 
-df = df %>% select(-comments)
-df = df %>% 
-    pmap(function(country, state, ...){
-                if(country != "United States") state = "None"
+{
+    df = df %>% select(-comments)
+    df = df %>% 
+        pmap(function(country, state, ...){
+                 if(country != "United States") state = "None"
 
-                return(data.frame(country, state, ...))
-              }) %>% 
+                 return(data.frame(country, state, ...))
+          }) %>% 
     bind_rows()  %>% 
     mutate(state = as.character(state)) %>% 
     mutate(state = as.factor   (state))
+}
 
 df$state  %>% levels() # now you can see "None"
 
 
 
-check_NA <- function(value) ifelse(is.na(value), "yes", "no")
+{
+    check_NA <- function(value) ifelse(is.na(value), "yes", "no")
 
-df = df %>% 
-    mutate(
-           isNAstate          = check_NA(state),
-           isNAself_employed  = check_NA(self_employed),
-           isNAwork_interfere = check_NA(work_interfere),
-           ) %>% 
+    df = df %>% 
+        mutate(
+               isNAstate          = check_NA(state),
+               isNAself_employed  = check_NA(self_employed),
+               isNAwork_interfere = check_NA(work_interfere),
+               ) %>% 
     mutate(
            isNAstate          = as.factor(isNAstate),
            isNAself_employed  = as.factor(isNAself_employed),
            isNAwork_interfere = as.factor(isNAwork_interfere),
 
     )
+}
 
 
 df %>% glimpse()
 
-impute_recipe  <- df %>% 
-    recipe(treatment ~ .) %>% 
-    step_knnimpute(all_predictors(), neighbors = 3)
+{
+    impute_recipe  <- df %>% 
+        recipe(treatment ~ .) %>% 
+        step_knnimpute(all_predictors(), neighbors = 3)
 
-imputed_df = impute_recipe %>% 
-    prep() %>% 
-    juice()
+    imputed_df = impute_recipe %>% 
+        prep() %>% 
+        juice()
 
-imputed_df %>% glimpse()
-imputed_df %>% 
-    filter(isNAstate == "yes") %>% 
-    head()
+    imputed_df %>% glimpse()
+    imputed_df %>% 
+        filter(isNAstate == "yes") %>% 
+        head()
 
 
-imputed_df %>% summarise_all(~ sum(is.na(.)))
-df$country  %>% levels()
-imputed_df$country
+    imputed_df %>% summarise_all(~ sum(is.na(.)))
 
-df = imputed_df
+    df = imputed_df
+}
 df %>% glimpse()
 
 # 2 What are features the most important? ================================================
 # Here I will use Boruta algorithm and correlation matrix
 library(Boruta)
 
-boruta.df <- Boruta(treatment ~ ., data=df, doTrace=2, maxRuns=250)
+boruta.df <- Boruta(treatment ~ ., data=df, doTrace=2, maxRuns=300)
 print(boruta.df)
 
 {
@@ -171,15 +206,6 @@ print(boruta.df)
 }
 
 
-df %>% 
-    filter(age > 0) %>% 
-    filter(age < 100) %>% 
-    group_by(age) %>% 
-    summarise(n = n()) %>% 
-    arrange(desc(n)) %>% 
-    ggplot(aes(age, n)) +
-    geom_point()
-    geom_line()
 
 
 
